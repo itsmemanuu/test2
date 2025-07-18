da828b758be88fcf702702d570b6402e3e25c007
Manuela Pacheco Malagón <98624209+itsmemanuu@users.noreply.github.com>
2025-07-17 20:34:09 -0500

    Create upStats.py

---
+++ upStats.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+post_url = "http://34.31.27.183:8000/data"
+
+with open("report.json", "r") as f:
+    payload = json.load(f)  {}
+
+response = requests.post(post_url, json=payload)
+print(response.json())
