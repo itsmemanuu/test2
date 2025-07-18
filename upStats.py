8aa497dae61c514a6cdd75e71d71c0899556eea6
Manuela Pacheco Malagón <98624209+itsmemanuu@users.noreply.github.com>
2025-07-17 20:37:47 -0500

    Update upStats.py

--- upStats.py
+++ upStats.py
@@ -4,7 +4,7 @@ import json
 post_url = "http://34.31.27.183:8000/data"
 
 with open("report.json", "r") as f:
-    payload = json.load(f)  {}
+    payload = json.load(f)
 
 response = requests.post(post_url, json=payload)
 print(response.json())
